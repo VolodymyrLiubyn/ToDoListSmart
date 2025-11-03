44f856692df343e907bb852fa19aa15048df7273
Petro <filipcenkopetro@gmal.com>
2025-11-03 15:15:48 +0200

    feat: 1 version of components, without logic

--- src/shared/ui/common/VInput.vue
+++ src/shared/ui/common/VInput.vue
@@ -1,18 +1,22 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div class=" ">
-    <input
-      class=" "
-      type="text"
-      placeholder="Enter text here"
+  <div class="flex flex-col gap-1">
+    <label
+      v-if="$slots.label"
+      class="font-medium text-gray-700"
     >
-    <slot />
+      <slot name="label" />
+    </label>
+
+    <input
+      class="border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    >
+
+    <p
+      v-if="$slots.error"
+      class="text-red-600 text-sm"
+    >
+      <slot name="error" />
+    </p>
   </div>
 </template>
 
-<style scoped>
-
-</style>
--- src/shared/ui/common/VLoader.vue
+++ src/shared/ui/common/VLoader.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 
+
 </script>
 
 <template>
+  <div class="flex flex-col items-center justify-center p-4">
+    <div
+      class="animate-spin rounded-full h-10 w-10
+     border-4 border-blue-500 border-t-transparent mb-2"
+    />
+
+    <slot />
+  </div>
 </template>
-
-<style scoped>
-
-</style>
