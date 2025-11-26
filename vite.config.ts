70b84bcafe69acacee819bc956ce4b9c76d7ee0c
Volodymyr Liubyn <vovalu88@gmail.com>
2025-11-27 00:09:29 +0200

    First try

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,33 @@
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 
-export default ({mode}: any) => {
-    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    return defineConfig({
-        plugins: [vue()],
-        resolve: {
-            alias: {
-                "@": fileURLToPath(new URL("./src", import.meta.url)),
-            },
+  return defineConfig({
+    base: "/ToDoListSmart/", // 👈 важливо для GitHub Pages
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    define: { "process.env": {} },
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build: {
+      chunkSizeWarningLimit: 1100,
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["vue", "vue-router"],
+          },
         },
-        define: {"process.env": {}},
-        server: {
-            port: 3000,
-            open: true,
-        },
-        build: {
-            chunkSizeWarningLimit: 1100,
-            sourcemap: true,
-            rollupOptions: {
-                output: {
-                    manualChunks: {
-                        vendor: ["vue", "vue-router"],
-                    },
-                },
-            },
-        },
-    })
+      },
+    },
+  })
 }
